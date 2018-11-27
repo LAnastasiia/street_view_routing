9cbf0165198b92517467b8457f219b9ad2d0e713
KazanYura <kazan.yura99@gmail.com>
2018-11-27 13:58:39 +0200

    database_edited

--- database/reader.py
+++ database/reader.py
@@ -7,7 +7,7 @@ def create_db():
         conn = sqlite3.connect("index.db")
         c = conn.cursor()
         c.execute('''CREATE TABLE IF NOT EXISTS streets
-                     (begin int, end int, time int, len int)''')
+                     (begin int, end int, time int, len int, is_visited int)''')
         c.execute('''CREATE TABLE IF NOT EXISTS junctions (id int,latitude real, longitude real, degree int)''')
         conn.commit()
     except Error as e:
@@ -18,21 +18,22 @@ def create_db():
 
 def read_from_txt():
     id = 0
-    f = open("input_data.txt", "r")
+    f = open("street_view_routing/database/input_data.txt", "r")
     first_line = f.readline().split()
     num_of_junctions = int(first_line[0])
-    num_of_streets = int(first_line[1])
-    for i in range(num_of_junctions):
-        write_junction(id, f.readline())
+    lines = f.readlines()
+    for i in lines[num_of_junctions:]:
+        write_street(i)
+    for i in lines[0:num_of_junctions]:
+        write_junction(id, i)
         id += 1
-    for i in range(num_of_streets):
-        write_street(f.readline())
 
 
 def write_junction(id, text):
     conn = sqlite3.connect("index.db")
     c = conn.cursor()
-    c.execute("INSERT INTO junctions VALUES ({},{},{},{})".format(id, text.split()[0], text.split()[1], 0))
+    degree = c.execute("SELECT COUNT(*) FROM streets WHERE begin = {}".format(id)).fetchall()
+    c.execute("INSERT INTO junctions VALUES ({},{},{},{})".format(id, text.split()[0], text.split()[1], degree[0][0]))
     conn.commit()
     conn.close()
 
@@ -41,13 +42,13 @@ def write_street(text):
     conn = sqlite3.connect("index.db")
     c = conn.cursor()
     if int(text.split()[2]) == 1:
-        c.execute("INSERT INTO streets VALUES ({},{},{},{})".format(text.split()[0], text.split()[1],
-                                                                    text.split()[3], text.split()[4]))
+        c.execute("INSERT INTO streets VALUES ({},{},{},{},{})".format(text.split()[0], text.split()[1],
+                                                                    text.split()[3], text.split()[4], 0))
     else:
-        c.execute("INSERT INTO streets VALUES ({},{},{},{})".format(text.split()[0], text.split()[1],
-                                                                    text.split()[3], text.split()[4]))
-        c.execute("INSERT INTO streets VALUES ({},{},{},{})".format(text.split()[1], text.split()[0],
-                                                                    text.split()[3], text.split()[4]))
+        c.execute("INSERT INTO streets VALUES ({},{},{},{},{})".format(text.split()[0], text.split()[1],
+                                                                    text.split()[3], text.split()[4], 0))
+        c.execute("INSERT INTO streets VALUES ({},{},{},{},{})".format(text.split()[1], text.split()[0],
+                                                                    text.split()[3], text.split()[4], 0))
     conn.commit()
     conn.close()
 
